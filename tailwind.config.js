e26cdd6d329e0333fbf1612395273f45e97271e9
Gustavo Gava <gustavogava123@hotmail.com>
2023-05-31 09:32:52 -0300

    feat: created button component and its stories

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			},
+
+			colors: {
+				primary: {
+					DEFAULT: "#1edddd",
+					hover: "#1ec2cf",
+				},
+				secondary: {
+					DEFAULT: "#1e1e1e",
+					hover: "#2e2e2e",
+				},
+			},
+		},
+	},
+	plugins: [],
+};
